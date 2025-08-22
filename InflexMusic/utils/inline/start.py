118959125c542df7934e567e84fb2e79b5d0f371
eko777e <replitserver445@gmail.com>
2025-08-22 05:02:39 +0400

    Update start.py

--- InflexMusic/utils/inline/start.py
+++ InflexMusic/utils/inline/start.py
@@ -7,13 +7,8 @@ def start_panel(_):
     buttons = [
         [ 
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_13"], url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
         ],
-        [
-            InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
-            ),
-        ]
     ]
     return buttons
 
@@ -21,21 +16,22 @@ def start_panel(_):
 def private_panel(_):
     buttons = [
         [
-            InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users",
-            )
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHANNEL)
+            InlineKeyboardButton(text=_["S_B_13"], url=config.SUPPORT_GROUP),
         ],
         [  
             InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
             InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG"),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_13"], url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(
+                text=_["S_B_3"],
+                url=f"https://t.me/{app.username}?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users",
+            ),
         ],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_14"], url=config.ELCAN_GROUP),
         ],
     ]
     return buttons
